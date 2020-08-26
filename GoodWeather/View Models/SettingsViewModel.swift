f1ea212a24546e97bda1961bd2e80d8ed3ea5fbd
Juan Dorado <fdorado985@gmail.com>
2020-08-26 18:51:46 -0500

    Implementing ViewModel for SettingsController

---
+++ GoodWeather/View Models/SettingsViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewModel.swift
+//  GoodWeather
+//
+//  Created by Juan Francisco Dorado Torres on 26/08/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+enum Unit: String, CaseIterable {
+  case celsius = "metric"
+  case fahrenheit = "imperial"
+  case kelvin = "kelvin"
+}
+
+extension Unit {
+
+  var displayName: String {
+    switch self {
+    case .celsius:
+      return "Celcius"
+    case .fahrenheit:
+      return "Fahrenheit"
+    case .kelvin:
+      return self.rawValue
+    }
+  }
+}
+
+struct SettingsViewModel {
+
+  private let units = Unit.allCases
+
+  var numberOfUnits: Int {
+    return units.count
+  }
+}
