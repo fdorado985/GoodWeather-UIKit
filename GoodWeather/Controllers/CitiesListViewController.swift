b62810e35398b605077469be05cc5b8cbe2a04b2
Juan Dorado <fdorado985@gmail.com>
2020-08-26 16:59:25 -0500

    display hardcoded weather and change the cell style

--- GoodWeather/Controllers/CitiesListViewController.swift
+++ GoodWeather/Controllers/CitiesListViewController.swift
@@ -11,3 +11,23 @@ import UIKit
 class CitiesListViewController: UITableViewController {
 
 }
+
+// MARK: - UITableView Delegate & DataSource
+
+extension CitiesListViewController {
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 8
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+    cell.textLabel?.text = "Houston"
+    cell.textLabel?.font = .preferredFont(forTextStyle: .callout)
+    cell.textLabel?.textColor = .systemGray
+    cell.detailTextLabel?.text = "28°"
+    cell.detailTextLabel?.font = .preferredFont(forTextStyle: .title1)
+    cell.detailTextLabel?.textColor = .systemRed
+    return cell
+  }
+}
